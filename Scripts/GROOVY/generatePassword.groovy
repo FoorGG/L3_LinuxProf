eeea9feec18d05ca9d67c5237b2077e34469ae1c
povarintsev-av <difkust@mail.ru>
2025-01-24 17:52:51 +1000

    Edit groovy and playbook

--- Scripts/GROOVY/generatePassword.groovy
+++ Scripts/GROOVY/generatePassword.groovy
@@ -1,22 +1,36 @@
 import java.security.SecureRandom
 import groovy.transform.Field
+import groovy.json.JsonOutput
 
+// Определение переменных
+String user_name = "otus_admin"
+String user_id = generatePassword(5)
+String passwd = generatePassword(25)
+String user = user_name + "-" + user_id
 
-String user = "otus_admin"
-String passwd =  generatePassword(25)
-//def pswd = new File('C:/Users/Поваринцевы/Desktop/Работа Саша/vagrant/pswd.txt')
-def pswd = new File('/tmp/pswd.txt')
-pswd.createNewFile()
-pswd.withWriter {currentObject ->
+// Создание объекта для хранения данных
+def userData = [
+        username: user,
+        password: passwd
+]
+
+// Запись данных в текстовый файл
+def pswdFile = new File('/tmp/pswd.txt')
+pswdFile.createNewFile()
+pswdFile.withWriter { currentObject ->
     currentObject.writeLine "${user}"
     currentObject.writeLine "${passwd}"
 }
 println("Password for ${user}: \"${passwd}\"")
 
+// Запись данных в JSON файл
+def jsonFile = new File('/tmp/user_data.json')
+jsonFile.text = JsonOutput.toJson(userData)
+println("User data saved to ${jsonFile.absolutePath}")
+
 @Field final def REGULAR_PASSWORD_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()~?.,'
-@Field final def PASSWORD_CHARACTERS = REGULAR_PASSWORD_CHARACTERS + '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
 
-
+// Функция генерации пароля
 def generatePassword(length) {
     if (length < 0) throw new IllegalArgumentException("length($length) is < 0")
 
